5dc1c952281eb40af128880dd19ac2eac455e6c4
Chicco Khoza <khozachicco@gmail.com>
2021-08-02 15:36:01 +0200

    I modified helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,5 @@
-// console.log('Hello World');
-console.log('Git Is Awesome');
+// // console.log('Hello World');
+// console.log('Git Is Awesome');
+
+let name = prompt("What is your name?");
+console.log("Hi "+ name)
